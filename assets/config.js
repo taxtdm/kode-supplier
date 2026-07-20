9b120aaa14c71de61d1fe30aa923fd995e7f460d
taxtdm <gitaaf.tunas@gmail.com>
2026-07-20 13:50:08 +0700

    Update API_URL to the deployed Google Apps Script URL

--- assets/config.js
+++ assets/config.js
@@ -1,3 +1,3 @@
 // Ganti URL di bawah dengan URL Web App hasil deploy Google Apps Script (Code.gs).
 // Contoh: https://script.google.com/macros/s/AKfycb.................../exec
-const API_URL = 'PASTE_URL_WEB_APP_DISINI';
+const API_URL = 'https://script.google.com/macros/s/AKfycbyclR6AQf5tGD_p3m8W9P4weJR8fFPcphoStdkDKd0Y2VxDomdKx-fQg_KynojJWH9G/exec';
